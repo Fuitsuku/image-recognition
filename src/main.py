5d6a68fedfa5969f307d7f160f2c872ec5b9c6f3
Fuitsuku <mitsulearnscode@gmail.com>
2023-11-06 14:44:57 -0700

    Followed TensorFlow Tutorial to understand high-level concept

--- src/main.py
+++ src/main.py
@@ -12,9 +12,22 @@ def unpickle(file):
 
 
 def main():
-    file_path = '/Users/mitsuakifukuzaki/Desktop/Hub/Programming/Python_Project/Image_Recognition/images/data_batch_1'
-    unPickledData = unpickle(file_path)
-    print(unPickledData)
+    (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+
+    train_images, test_images = train_images / 255.0, test_images / 255.0
+
+    class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+    plt.figure(figsize = ( 10, 10) )
+    for i in range(25):
+        plt.subplot(5, 5, i + 1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.grid(False)
+        plt.imshow(train_images[i])
+        plt.xlabel(class_names[train_labels[i][0]])
+    plt.show()
+
     
 
 if __name__ == "__main__":
