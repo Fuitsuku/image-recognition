6730a0ad3e7ed1d4c9f2e51808f45795f9d5cee5
Fuitsuku <mitsulearnscode@gmail.com>
2023-11-06 14:28:08 -0700

    Converted to TensorFlow

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import numpy as np
-from PIL import Image
-import io
+import tensorflow as tf
+
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
 
 def unpickle(file):
     import pickle
@@ -8,21 +10,11 @@ def unpickle(file):
         data = pickle.load(fo, encoding='bytes')
     return data
 
-def readImages(image_data):
-    for image_bytes in image_data[b'data']:
-        # Create a BytesIO object to read image bytes
-        image_io = io.BytesIO(image_bytes)
-        im = Image.open(image_io)
-        print(im.format)
-        print(im.size)
-        print(im.mode)
-        im.show()
 
 def main():
     file_path = '/Users/mitsuakifukuzaki/Desktop/Hub/Programming/Python_Project/Image_Recognition/images/data_batch_1'
     unPickledData = unpickle(file_path)
     print(unPickledData)
-    readImages(unPickledData)
     
 
 if __name__ == "__main__":
