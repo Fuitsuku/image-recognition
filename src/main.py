ee1f00c8083ef806509e4e72f62ba8dbea429e12
Fuitsuku <mitsulearnscode@gmail.com>
2023-11-08 22:42:09 -0700

    Loaded data_batch_1 into numpy matrices. Currently working on formating data into uasable format

--- src/main.py
+++ src/main.py
@@ -1,7 +1,5 @@
 import numpy as np
 import tensorflow as tf
-
-from tensorflow.python.keras import datasets, layers, models
 import matplotlib.pyplot as plt
 
 def unpickle(file):
@@ -10,17 +8,29 @@ def unpickle(file):
         data = pickle.load(fo, encoding='bytes')
     return data
 
+def unpackage_images(filepath):
+    label_key = 'labels'
+    data_intermediate = unpickle(filepath)
+    data_decoded = {}
+
+    for i, j in data_intermediate.items():
+        data_decoded[i.decode('utf8')] = j
+    data_intermediate = data_decoded
+
+    data = data_intermediate["data"]
+    labels = data_intermediate[label_key]
+
+    data = data.reshape(data.shape[0], 3, 32, 32)
+
+    return data, labels
 
 def main():
     image_file_path = '/Users/mitsuakifukuzaki/Desktop/Hub/Programming/Python_Project/Image_Recognition/images/data_batch_1'
-    data = unpickle(image_file_path)
-    data_decoded = {}
+    data = unpackage_images(image_file_path)
+    labels = data[1]
 
-    for i, j in data.items():
-        data_decoded[i.decode('UTF-8')] = j
-
-    print(data_decoded)
-
+    print(data)
+    print(type(data))
     
 
 if __name__ == "__main__":
