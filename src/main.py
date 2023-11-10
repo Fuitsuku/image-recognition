388c5a879cafb780a12e6fdf7d35759810840f21
Fuitsuku <mitsulearnscode@gmail.com>
2023-11-09 17:03:09 -0700

    Imported an image of russett potatoes using PIL.Image, np.array, and matplotlib.pyplot

--- src/main.py
+++ src/main.py
@@ -1,35 +1,18 @@
 import numpy as np
-import tensorflow as tf
+from PIL import Image
 import matplotlib.pyplot as plt
 
-def unpickle(file):
-    import pickle
-    with open(file, 'rb') as fo:
-        data = pickle.load(fo, encoding='bytes')
-    return data
 
-def unpackage_images(filepath):
-    label_key = 'labels'
-    data_intermediate = unpickle(filepath)
-    data_decoded = {}
-
-    for i, j in data_intermediate.items():
-        data_decoded[i.decode('utf8')] = j
-    data_intermediate = data_decoded
-
-    data = data_intermediate["data"]
-    labels = data_intermediate[label_key]
-
-    data = data.reshape(data.shape[0], 3, 32, 32) #Restructures data into RGB, 32 by 32 cells.
-                                                  # 10000 x 3072 -> 10000 x 3 x 32 x 32
-
-    return data, labels
+fpath = '/Users/mitsuakifukuzaki/Desktop/Hub/Programming/Python_Project/Image_Recognition/images/potato.jpg'
 
 def main():
-    image_file_path = '/Users/mitsuakifukuzaki/Desktop/Hub/Programming/Python_Project/Image_Recognition/images/data_batch_1'
-    (data, labels) = unpackage_images(image_file_path)
+    image = Image.open(fpath)
+    image_array = np.array(image)
 
-
+    print(image_array.shape)
+    plt.imshow(image_array)
+    plt.axis('off')
+    plt.show()
     
 
 if __name__ == "__main__":
