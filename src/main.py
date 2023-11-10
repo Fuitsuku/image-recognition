bf6970f7a161291bd09ca84acf9e58f582641f5f
Fuitsuku <mitsulearnscode@gmail.com>
2023-11-09 18:25:36 -0700

    Partitioned 9955 images into 10 folders containing 1000 images. Github has a 1000 file limit

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 import numpy as np
+import os
+import shutil
 from PIL import Image
 import matplotlib.pyplot as plt
 
 
-fpath = '/Users/mitsuakifukuzaki/Desktop/Hub/Programming/Python_Project/Image_Recognition/images/2A2X.png'
+filepath = '/Users/mitsuakifukuzaki/Desktop/Hub/Programming/Python_Project/Image_Recognition/captcha_images'
 
 def showImage(fpath):
     image = Image.open(fpath)
@@ -14,10 +16,37 @@ def showImage(fpath):
     plt.axis('off')
     plt.show()
 
+def partitionImages(fpath):
+    file_names = os.listdir(fpath)
+    folder_names = ['batch_1', 'batch_2', 'batch_3', 
+                    'batch_4', 'batch_5', 'batch_6', 
+                    'batch_7', 'batch_8', 'batch_9', 
+                    'batch_10']
+
+    SRC_DIRECTORY = '/Users/mitsuakifukuzaki/Desktop/Hub/Programming/Python_Project/Image_Recognition/'
+    counter = 0
+    COUNTER_LIMIT = 1000
+    current_folder = 0
+
+    for file_name in file_names:
+        if counter == COUNTER_LIMIT:
+            counter = 0
+            current_folder += 1
+        source_file = SRC_DIRECTORY +'captcha_images/' + file_name
+        destination_file = SRC_DIRECTORY +'images/' + folder_names[current_folder]
+        
+        shutil.move(source_file, destination_file)
+        counter += 1
+
+def saveImageNames(fpath):
+    file_names = os.listdir(fpath)
+    print(len(file_names))
+
+    with open('image_names.txt', 'w') as txt_file:
+        for name in file_names:
+            txt_file.write(name + '\n')
 
 def main():
-    showImage(fpath)
-    
-
+    return
 if __name__ == "__main__":
     main()
